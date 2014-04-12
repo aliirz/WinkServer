2be7f4416d814aea6fd4fa92ad261b66d5716c30
aliirz <dejavu87@gmail.com>
2014-04-13 03:23:54 +0500

    sinatra-finally

--- server.rb
+++ server.rb
@@ -1,16 +1,44 @@
-require "em-websocket"
+require 'sinatra'
+require 'sinatra-websocket'
 
-EventMachine.run do
-  @channel = EM::Channel.new
+set :server, 'thin'
+set :sockets, []
 
-  EventMachine::WebSocket.start(host: "0.0.0.0", port: 8081, debug: true) do |ws|
-    ws.onopen do
-      sid = @channel.subscribe { |msg| ws.send(msg) }
-      @channel.push("#{sid} connected")
-
-      ws.onmessage { |msg| @channel.push("<#{sid}> #{msg}") }
-
-      ws.onclose { @channel.unsubscribe(sid) }
+get '/' do
+  if !request.websocket?
+    erb :index
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("Hello World!")
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+      end
+      ws.onclose do
+        warn("websocket closed")
+        settings.sockets.delete(ws)
+      end
     end
   end
 end
+
+
+
+# require "em-websocket"
+#
+# EventMachine.run do
+#   @channel = EM::Channel.new
+#
+#   EventMachine::WebSocket.start(host: "0.0.0.0", port: 8080, debug: true) do |ws|
+#     ws.onopen do
+#       sid = @channel.subscribe { |msg| ws.send(msg) }
+#       @channel.push("#{sid} connected")
+#
+#       ws.onmessage { |msg| @channel.push("<#{sid}> #{msg}") }
+#
+#       ws.onclose { @channel.unsubscribe(sid) }
+#     end
+#   end
+# end
